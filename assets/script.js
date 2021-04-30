088c174175d1ffbe06d48e301e8b4ab39f2ae254
michaelzadra27 <michaelzadra27@gmail.com>
2021-04-29 18:51:21 -0700

    fleshed out HTML

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//Create form that grabs value of city search
+//Retrieve data from API and append object elements to Container
+//
+
+
+var requestUrl = "api.openweathermap.org/data/2.5/weather?q="
+
+var apiKey  = "9b10ba0f1eb331935dd772c9d1676bf9"
+
+var cityKey = "innerhtmlvalue"
